0d8203d8e3d67f1c1b938b1e1d2f8f3ed78edec0
Graham Wright <graham@opusaffair.com>
2018-05-25 21:36:46 -0400

    add manual similar call

--- server.js
+++ server.js
@@ -1,27 +1,20 @@
-require('dotenv').config()
-const express = require('express')
-const { ApolloServer, gql, makeExecutableSchema, ForbiddenError, AuthenticationError } = require('apollo-server')
-const { registerServer } = require('apollo-server-express')
-const jwt = require('express-jwt')
-const cors = require('cors')
-const jwks = require('jwks-rsa')
+require('dotenv').config();
+const express = require('express');
+const { ApolloServer, gql, makeExecutableSchema, ForbiddenError, AuthenticationError } = require('apollo-server');
+const { registerServer } = require('apollo-server-express');
 const neo4j = require('neo4j-driver').v1;
 const {neo4jgraphql} = require('neo4j-graphql-js');
 
-
-
-const app = express()
+const app = express();
 
 const typeDefs = gql`
-  directive @isAuthenticated on FIELD | FIELD_DEFINITION
-  directive @hasRole(role: String) on FIELD | FIELD_DEFINITION
 
   type Movie {
+    id: ID!
     title: String
-    releaseDate: Float
     tagline: String
-    actors(first: Int = 3, offset: Int = 0): [Person] @relation(name: "ACTED_IN", direction:"IN")
-    similar(first: Int = 3, offset: Int = 0): [Movie] @cypher(statement: "WITH {this} AS this MATCH (this)--(:Genre)--(o:Movie) RETURN o LIMIT {limit}")
+    actors(first: Int = 3, offset: Int = 0): [Person] @relation(name: "ACTS_IN", direction:"IN")
+    similar(first: Int = 3, offset: Int = 0): [Movie]
     degree: Int @cypher(statement: "WITH {this} AS this RETURN SIZE((this)--())")
   }
 
@@ -33,87 +26,48 @@ const typeDefs = gql`
 
   type Query {
     hello: String,
-    helloUser: String,
-    authentication: String @isAuthenticated,
-    permission: String @hasRole(role:"Admin"),
-    listMovies(title: String, first: Int, offset: Int): [Movie]
+    listMovies(title: String, first: Int, offset: Int): [Movie],
+    movies(limit: Int!): [Movie]
   }
 
 `;
 
-const directiveResolvers = {
-  isAuthenticated: (next, source, args, ctx) => {
-    const user = ctx.user
-    if (user) return next()
-    throw AuthenticationError("You must be logged in to see this");
-  },
-  hasRole: (next, source, { role }, ctx) => {
-    const user = ctx.user
-    if (user){
-      const roles = ctx.roles
-      if (roles.includes(role)) return next()
-      throw ForbiddenError(`Must have ${role} permissions to view this field`)
-    }
-    throw AuthenticationError(`Must be logged in as ${role} to view this field`)
-  },
-}
 
 const resolvers = {
   Query: {
     hello: () => 'Hello world!',
-    helloUser (root, args, ctx, info) {
-      var d = new Date();
-      const user = ctx.user
-
-      const name = user ? user.nickname : 'there'
-      return `Hello ${name} (${d})`
-    },
-    permission: () => 'Looks like you have the right permissions',
-    authentication: () => "Looks like you're logged in",
     listMovies(object, params, ctx, resolveInfo) {
       // neo4jgraphql inspects the GraphQL query and schema to generate a single Cypher query
       // to resolve the GraphQL query. Assuming a Neo4j driver instance exists in the context
       // the query is executed against Neo4j
      return neo4jgraphql(object, params, ctx, resolveInfo);
-   }
+    },
+    movies(_, params, ctx) {
+      let session = ctx.driver.session();
+      let query = "MATCH (movie:Movie) RETURN movie LIMIT $limit;"
+      return session.run(query, params)
+        .then( result => { return result.records.map(record => { return record.get("movie").properties })})
+    }
+
+  },
+  Movie:{
+    similar: (object, _, context) => {
+      let session = context.driver.session();
+      let params = {id: object.id, limit: 5};
+      console.log(object);
+      let query = `
+				MATCH (m)
+        Where m.id = $id
+				Match (m)-->(o)
+				RETURN o LIMIT $limit;
+			`;
+      return session.run(query, params)
+        .then( result => { return result.records.map(record => { return record.get("o").properties })})
+    }
   }
-}
+};
 
-// enable CORS
-app.use(cors())
 
-// define auth middleware
-const auth = jwt({
-  secret: jwks.expressJwtSecret({
-    cache: true,
-    rateLimit: true,
-    jwksRequestsPerMinute: 5,
-    jwksUri: `${process.env.AUTH0_ISSUER}.well-known/jwks.json`
-  }),
-  credentialsRequired: false,
-  audience: process.env.AUTH0_AUDIENCE,
-  issuer: process.env.AUTH0_ISSUER,
-  algorithms: ['RS256']
-})
-
-const errorHandler = (err, req, res, next) => {
-  //Log all errors and continue
-  //Need to catch expired tokens
-  console.log(err.name+": "+err.message)
-  return next()
-}
-
-// add authentication middleware
-app.use(auth, errorHandler)
-
-// get roles for user...to be changed later
-function lookupRolesForUser(user){
-  var roles = []
-  if (user){
-    roles = user.roles
-  }
-  return roles
-}
 
 // update context
 const context = ({ req }) => {
@@ -122,15 +76,12 @@ const context = ({ req }) => {
   let driver;
   if (!driver){
   	// driver = neo4j.driver("bolt://localhost:7687", neo4j.auth.basic("neo4j", "password"))
-    driver = neo4j.driver(process.env.NEO4J_URI || "bolt://localhost:7687", neo4j.auth.basic(process.env.NEO4J_USERNAME || "neo4j", process.env.NEO4J_PASSWORD || "password"))
-
+    driver = neo4j.driver(
+      process.env.NEO4J_URI || "bolt://localhost:7687",
+      neo4j.auth.basic(process.env.NEO4J_USERNAME || "neo4j",
+      process.env.NEO4J_PASSWORD || "password"))
   }
-
-  //add user object to context
-  const user = req.user
-  const roles = lookupRolesForUser(user);
-
-  return { user, roles, driver }
+  return { driver }
 };
 
 
@@ -139,8 +90,7 @@ const context = ({ req }) => {
 
 const schema = makeExecutableSchema({
   typeDefs,
-  resolvers,
-  directiveResolvers
+  resolvers
 });
 
 const server = new ApolloServer({
@@ -153,4 +103,4 @@ registerServer({ server, app })
 // normal ApolloServer listen call but url will contain /graphql
 server.listen().then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`)
-})
+});
